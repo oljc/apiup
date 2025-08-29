f0d6e722a55332c84c4c948dcf350954e81b34e2
lryouyu <165154922+lryouyu@users.noreply.github.com>
2025-08-29 20:10:25 +0800

    feat(utils): add some function (#5)
    * feat(utils/index): add isEmpty function
    
    * feat(utils/index): add new type judgment

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,23 @@ export function isUndefined(obj: any): obj is undefined {
 export function isNull(obj: any): obj is null {
 	return obj === null;
 }
+
+export function isString(obj: any): obj is string {
+	return opt.call(obj) === '[object String]';
+}
+
+export function isNumber(obj: any): obj is number {
+	return opt.call(obj) === '[object Number]';
+}
+
+export function isArray(obj: any): obj is any[] {
+	return opt.call(obj) === '[object Array]';
+}
+
+export function isDate(obj: any): obj is Date {
+	return opt.call(obj) === '[object Date]';
+}
+
+export function isObject(obj: any): obj is { [key: string]: any } {
+	return opt.call(obj) === '[object Object]';
+}
