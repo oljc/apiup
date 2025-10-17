619a01de88af0b1a41474dff3b4aea3b6daf9e76
李江辰 <ljc.byte@gmail.com>
2025-10-17 16:55:51 +0800

    refactor: remove next page

--- node-functions/[[default]].ts
+++ node-functions/[[default]].ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { contextMiddleware } from 'node-functions/middleware/context';
 import { corsMiddleware } from 'node-functions/middleware/cors';
-import xhs from './routes/[[xhs]]';
+import xhs from './routes/xhs';
 
 const app = new Hono().basePath('/');
 
@@ -9,7 +9,30 @@ app.use('*', corsMiddleware);
 app.use('*', contextMiddleware);
 
 app.get('/', (c) => {
-  return c.ok(null, '欢迎开放 API 服务')
+  return c.html(`
+    <!DOCTYPE html>
+    <html lang="zh-CN">
+    <head>
+      <meta charset="UTF-8">
+      <title>API 服务</title>
+      <style>
+        html, body {
+          height: 100%;
+          margin: 0;
+          font-family: sans-serif;
+          background-color: #000;
+          color: #fff;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>API Service Running!</h1>
+    </body>
+    </html>
+  `);
 });
 
 app.route('/xhs', xhs);
